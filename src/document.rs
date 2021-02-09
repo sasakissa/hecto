6e14c6d689cbf5df821d510a1792829d06c660d0
MitaRyosuke <sasakissa@gmail.com>
2021-02-09 16:00:28 +0900

    wip: chapter 5

--- src/document.rs
+++ src/document.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use crate::Row;
+use crate::{Position, Row};
 
 #[derive(Default)]
 pub struct Document {
@@ -30,5 +30,28 @@ impl Document {
 
     pub fn len(&self) -> usize {
         self.rows.len()
+    }
+
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y == self.len() {
+            let mut row = Row::default();
+            row.insert(0, c);
+            self.rows.push(row);
+        } else if at.y < self.len() {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.insert(at.x, c);
+        }
+    }
+
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.append(&next_row)
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.delete(at.x);
+        }
     }
 }
